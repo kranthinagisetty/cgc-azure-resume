495b6d23168ad9809eec6c312e8589c288eb1d1b
GPS <gwynethpena@outlook.com>
2021-04-05 15:41:59 -0400

    Added error handling to JS in frontend

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ View it live [here](https://www.gwynethpena.com)
 
 
 ![Diagram](img/diagram.png)
+
+## Front end resources
+
+- Since I am a terrible designer, I used this template to create the site. [HTML + CSS template](https://www.styleshout.com/free-templates/ceevee/)
+- How to make the API call from the Javascript code. 
--- frontend/main.js
+++ frontend/main.js
@@ -2,19 +2,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
 const functionApi = 'https://getresumecounterfunctionapp.azurewebsites.net/api/GetResumeCounter?code=jp4rx3wuLEzaXvSFeBKaq08RGiVCwRtpAA1Cdxdc15c9ZGjM79tWjg=='; 
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi, {
-        mode: 'cors',
-    })
+    fetch(localApi)
     .then(response => {
         return response.json()
     })
-    .then(res => {
-        const count = res.count;
-        document.getElementById('counter').innerText =count;
-    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
     return count;
 }
